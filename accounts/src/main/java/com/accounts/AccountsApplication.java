b2f43b031cd34ae8c790047e95322c73fa342d83
Tihamer Sebok <tihamer.sebok@endava.com>
2022-11-03 15:14:47 +0200

    micrometer, prometheus, grafana

--- accounts/src/main/java/com/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/accounts/AccountsApplication.java
@@ -1,15 +1,15 @@
 package com.accounts;
 import com.accounts.model.Accounts;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+
 
 import java.time.LocalDate;
 
@@ -20,13 +20,11 @@ public class AccountsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
+	}
 
-		Accounts accounts1 = new Accounts();
-		accounts1.setAccountType("Economy");
-		accounts1.setBranchAddress("New York");
-		accounts1.setCreateDt(LocalDate.parse("2022-10-16"));
-		accounts1.setAccountNumber(1L);
-		accounts1.setCustomerId(1);
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry){
+		return new TimedAspect(registry);
 	}
 
 }
